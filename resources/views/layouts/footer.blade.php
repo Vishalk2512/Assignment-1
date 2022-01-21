a5ed99f0692bb268590a85f5d5d58b67059c503a
Vishal Vansjariya <vishalvansjariya2512@gmail.com>
2022-01-21 08:41:43 +0530

    Modified Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,54 +2,54 @@
 <html lang="en">
 
 <head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<link rel="stylesheet" href="css/main.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
 
-		<footer class="bg-dark text-center text-white">
-				<!-- Grid container -->
-				<div class="container p-3 pb-0">
-						<!-- Section: Social media -->
-						<section class="mb-3">
-								<!-- Github -->
-								<a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Vishalk2512?tab=repositories"
-										role="button"><span class="footer-links"><i class="fab fa-github"></i></span></a>
+	<footer class="bg-dark text-center text-white">
+		<!-- Grid container -->
+		<div class="container p-3 pb-0">
+			<!-- Section: Social media -->
+			<section class="mb-3">
+				<!-- Github -->
+				<a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Vishalk2512?tab=repositories"
+					role="button"><span class="footer-links"><i class="fab fa-github"></i></span></a>
 
-								<!-- Twitter -->
-								<a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/Vishalk2512" role="button"><span
-												class="footer-links"><i class="fab fa-twitter"></i></span></a>
+				<!-- Twitter -->
+				<a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/Vishalk2512" role="button"><span
+						class="footer-links"><i class="fab fa-twitter"></i></span></a>
 
-								<!-- Instagram -->
-								<a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/vishalk2512002/"
-										role="button"><span class="footer-links"><i class="fab fa-instagram"></i></span></a>
+				<!-- Instagram -->
+				<a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/vishalk2512002/"
+					role="button"><span class="footer-links"><i class="fab fa-instagram"></i></span></a>
 
-								<!-- Linkedin -->
-								<a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/vishalk2512/"
-										role="button"><span class="footer-links"><i class="fab fa-linkedin-in"></i></span></a>
+				<!-- Linkedin -->
+				<a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/vishalk2512/"
+					role="button"><span class="footer-links"><i class="fab fa-linkedin-in"></i></span></a>
 
-								<!-- Facebook -->
-								<a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/Vishalk2512/"
-										role="button"><span class="footer-links"><i class="fab fa-facebook-f"></i></span></a>
-
-
+				<!-- Facebook -->
+				<a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/Vishalk2512/"
+					role="button"><span class="footer-links"><i class="fab fa-facebook-f"></i></span></a>
 
 
-						</section>
-						<!-- Section: Social media -->
-				</div>
-				<!-- Grid container -->
 
-				<!-- Copyright -->
-				<div class="text-center p-3" style="background-color: rgba(0, 0, 0); color: white">
-						© 2020 Copyright |
-						<a class="text-white" href="/">Vishal Vansjariya</a>
-				</div>
-				<!-- Copyright -->
-		</footer>
+
+			</section>
+			<!-- Section: Social media -->
+		</div>
+		<!-- Grid container -->
+
+		<!-- Copyright -->
+		<div class="text-center p-3" style="background-color: rgba(0, 0, 0); color: white">
+			© 2022 Copyright |
+			<a class="text-white" href="/">Vishal Vansjariya</a>
+		</div>
+		<!-- Copyright -->
+	</footer>
 
 </body>
 
